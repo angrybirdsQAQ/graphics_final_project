c4244307e6c80124775b42ca03d27ba7e84351f5
angrybirdsQAQ <d15188qwerty@gmail.com>
2018-06-21 20:07:27 +0800

    Add files via upload
    ttttest

--- Assets/fragment.fs.glsl
+++ Assets/fragment.fs.glsl
@@ -26,23 +26,66 @@ uniform float pow_spec;
 
 uniform sampler2D tex;
 
+
+//skybox
+uniform samplerCube skybox;
+uniform int state;
+in vec3 oripos;
+
+uniform vec3 eyepos;
+
+///////water
+uniform vec4 materAmbient, materSpecular;
+uniform vec4 lightDiffuse, lightAmbient, lightSpecular;
+uniform vec4 envirAmbient;
+
+varying vec2 texture_coord;
+
+varying vec3 normalVect;
+varying vec3 lightVect;
+varying vec3 eyeVect;
+varying vec3 halfWayVect;
+varying vec3 reflectVect;
+varying vec3 posVect;
+
+
+///////
+
+
 void main()
 {
-    // Normalize the incoming N, L and V vectors
-    vec3 N = normalize(vertexData.N);
-    vec3 L = normalize(vertexData.L);
-    vec3 V = normalize(vertexData.V);
-    vec3 H = normalize(L + V);
+	if (state == 0)
+	{
+		// Normalize the incoming N, L and V vectors
+		vec3 N = normalize(vertexData.N);
+		vec3 L = normalize(vertexData.L);
+		vec3 V = normalize(vertexData.V);
+		vec3 H = normalize(L + V);
 
-    // Compute the diffuse and specular components for each fragment
-    vec3 light_color = vec3(100.0f, 80.0f, 20.0f)/255;
-    vec3 ambient = k_amb * amb_color;
-    vec3 diffuse = max(dot(N, L), 0.0) * k_diff * light_color;
-    vec3 specular = pow(max(dot(N, H), 0.0), pow_spec) * k_spec * light_color;
+		// Compute the diffuse and specular components for each fragment
+		vec3 light_color = vec3(100.0f, 80.0f, 20.0f) / 255;
+		vec3 ambient = k_amb * amb_color;
+		vec3 diffuse = max(dot(N, L), 0.0) * k_diff * light_color;
+		vec3 specular = pow(max(dot(N, H), 0.0), pow_spec) * k_spec * light_color;
 
-    // Write final color to the framebuffer
-    vec3 texColor = texture(tex,vertexData.texcoord).rgb;
-    vec3 result = (ambient + diffuse + specular) * texColor;
-    fragColor = vec4(result, 1.0);
+		// Write final color to the framebuffer
+		vec3 texColor = texture(tex, vertexData.texcoord).rgb;
+		vec3 result = (ambient + diffuse + specular) * texColor;
+		fragColor = vec4(result, 1.0);
+	}
+	else if (state == 1)
+	{
+		fragColor = texture(skybox, oripos);
+		//fragColor = vec4(0.0, 1.0, 1.0, 1.0);
+	}
+	else if (state == 3)
+	{
+		vec3 I = normalize(posVect - eyepos);
+		vec3 R = reflect(I, normalize(normalVect));
+		fragColor = vec4(texture(skybox, R).rgb, 0.45) + vec4(0.76 / 2, 0.94 / 2, 0.94 / 2, 0.0);
+	}
+	else if (state == 2) {
+		fragColor = vec4(0.7, 0.8, 1.0, 1.0);
+	}
 
 }
--- Assets/vertex.vs.glsl
+++ Assets/vertex.vs.glsl
@@ -17,26 +17,79 @@ out VertexData
     vec2 texcoord;
 } vertexData;
 
+out vec3 oripos;
+
+//water
+attribute vec3 position;
+attribute vec3 normal;
+
+uniform mat4 modelViewMat;
+uniform mat4 perspProjMat;
+uniform mat3 normalMat;
+
+uniform float time;
+
+varying vec2 texture_coord;
+
+varying vec3 normalVect;
+varying vec3 lightVect;
+varying vec3 eyeVect;
+varying vec3 halfWayVect;
+varying vec3 reflectVect;
+varying vec3 posVect;
+//wwwww
+
+uniform int state;
+uniform vec3 eyepos;
+
 void main()
 {
+	if (state == 3) {
+		vec3 positionw = vec3(0.0, 1.0, 0.0);
+		gl_Position = um4p * um4mv * vec4(position, 1.0);
+		float tex_x = (position.x + time / 20.0) / 8.0 + 0.5;
+		float tex_y = 0.5 - (position.y + time / 25.0) / 5.0;
+		texture_coord = vec2(tex_x, tex_y);
 
-    // Calculate view-space coordinate
-    vec4 P = um4mv * vec4(iv3vertex, 1.0);
+		//vec3 eyePos = vec3(0.0, 0.0, 5.0);
+		vec3 eyePos2 = eyepos;
+		vec3 lightPos = vec3(1.0, 20.0, 1.0);
+		vec3 ptVertex = vec3(um4mv * vec4(position, 1.0));
 
-    // Calculate normal in view-space
-    // Using only the rotational components of mv_matrix
-    vertexData.N = mat3(um4mv) * iv3normal;
+		posVect.x = position.x;
+		posVect.y = position.z;
+		posVect.z = position.y;
+
+		eyeVect = normalize(eyePos2 - ptVertex);
+		lightVect = normalize(lightPos - ptVertex);
+		halfWayVect = eyeVect + lightVect;
+		normalVect = normalMat * normal;
+		reflectVect = 1.0 * eyeVect - 2.0 * dot(-1.0*eyeVect, normalVect) * normalVect;
+	}
+	else
+	{
 
 
-    // Calculate light vector
-    vertexData.L = mat3(um4mv) * light_pos - P.xyz;
+		// Calculate view-space coordinate
+		vec4 P = um4mv * vec4(iv3vertex, 1.0);
 
-    // Calculate view vector
-    vertexData.V = -P.xyz;
-
-    // Calculate the clip-space position of each vertex
-    gl_Position = um4p * P;
+		// Calculate normal in view-space
+		// Using only the rotational components of mv_matrix
+		vertexData.N = mat3(um4mv) * iv3normal;
 
 
-    vertexData.texcoord = iv2tex_coord;
+		// Calculate light vector
+		vertexData.L = mat3(um4mv) * light_pos - P.xyz;
+
+		// Calculate view vector
+		vertexData.V = -P.xyz;
+
+		// Calculate the clip-space position of each vertex
+		gl_Position = um4p * P;
+
+
+		vertexData.texcoord = iv2tex_coord;
+
+		oripos = iv3vertex;
+	}
 }
